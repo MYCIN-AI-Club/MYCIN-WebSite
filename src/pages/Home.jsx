e0af12d5e3296be797a89b52a97d36157a49e18b
tushar2024cs1028 <106095562+tushar2024cs1028@users.noreply.github.com>
2022-10-04 17:35:53 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import Firstsection from '../components/first-section/firstsection';
+import Secondsection from '../components/secondsection/secondsection';
+
+    
+const Home=()=>{
+    return (
+    <div>
+        
+        <Firstsection/>
+        <Secondsection/>
+        
+        
+    </div>
+    );
+};
+export default Home;
---
+++ src/pages/Team.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Team.css";
+import TeamMember from "../components/Team_Card/Team_Card_memb";
+
+ 
+const Team=()=>{
+    return (
+        <div className="Team">
+            <TeamMember/>
+        </div>
+    );
+}
+export default Team;
