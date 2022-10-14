a386c0059f1c981ba86eb5b634cbabc56feaa7fe
danshika42 <83402199+danshika42@users.noreply.github.com>
2022-10-14 17:06:36 +0530

    --updated event section

--- src/components/Events/EventsExplore/EventsExplore.jsx
+++ src/components/Events/EventsExplore/EventsExplore.jsx
@@ -10,16 +10,14 @@ const EventsExplore=(probs)=>{
       }
   },[probs.eventexploredatanew.authorized,navigate])
     return(
-        
         <div className="EventExplore">
-          
-           <div className="EventExplore_post">
-           {probs.eventexploredatanew.authorized?<Slider Images={probs.eventexploredatanew.imageSource} ></Slider>:null}
-           </div>
-           <div className="EventExplore_maincontent">
-            <h3 className="EventExplore_Title">Event Details</h3>
-            <h4 className="EventExplore_Name">{probs.eventexploredatanew.title}</h4>
-            <p className="EventExplore_Content">{probs.eventexploredatanew.text}
+            <div className="EventExplore_post">
+              {probs.eventexploredatanew.authorized?<Slider Images={probs.eventexploredatanew.imageSource} ></Slider>:null}
+            </div>
+            <div className="EventExplore_maincontent">
+              <h3 className="EventExplore_Title">Event Details</h3>
+              <h4 className="EventExplore_Name">{probs.eventexploredatanew.title}</h4>
+              <p className="EventExplore_Content">{probs.eventexploredatanew.text}
             </p>
            </div>
         </div>
